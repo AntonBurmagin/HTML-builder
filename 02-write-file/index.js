c8ca4e7a5cae1bd9cda71ffb4534a4baa93ade67
Anton Burmagin <burmagin.ant.s@gmail.com>
2023-05-08 19:03:09 +0600

    feat: add exit with Ctrl+C, fix code with use of write stream

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,27 +1,29 @@
 const fs = require('fs');
 const {stdin, stdout} = process;
 
+// creating empty file
 fs.writeFile("./02-write-file/text.txt", '', function(error) {
     if (error) console.log("Error on creating file №: " + error);
 });
 
-const writer = fs.createWriteStream;
+const writer = fs.createWriteStream("./02-write-file/text.txt");
 console.log("Hello! Write smth you want to store:");
 
 stdin.on('data', data => {
-    if(data.toString().toLowerCase() == 'exit\r\n') {
+    if(data.toString() === 'exit\r\n')
         process.exit(0);
-    }
-    fs.appendFile("./02-write-file/text.txt", data, function(error) {
-        if (error) console.log("Error on creating file №: " + error);
-    });
+    writer.write(data);
 });
 
 process.on('exit', code => {
     if (code === 0) {
         console.log('See u!');
     } else {
-        stderr.write(`Ошибка! Программа завершилась с кодом ${code}`);
+        stderr.write(`Error, program end with code: ${code}`);
     }
 });
 
+// ловим выход через Ctrl + C
+process.on('SIGINT', () => {
+    process.exit(0);
+});
