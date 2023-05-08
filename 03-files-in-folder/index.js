f41f2df4d0954e0e714b9ae8ff23192be7e2a5e0
Anton Burmagin <burmagin.ant.s@gmail.com>
2023-05-08 22:08:03 +0600

    feat: implement output of files info for task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,10 +1,17 @@
-const fsProm = require('fs/promises')
-const path = require('path');
-const dirInside = fsProm.readdir('./03-files-in-folder/secret-folder', {withFileTypes: true});
+const fsProm = require('fs/promises');
 
+const dirInside = fsProm.readdir('./03-files-in-folder/secret-folder', {withFileTypes: true});
+let result = [];
 dirInside.then(filenames => {
     for (let filename of filenames) {
         if(filename.isFile())
-            console.log(filename);
+            result.push(filename.name.split('.'));
+    }
+
+    for (let i=0; i<result.length; i++) {
+        const fileInfo = fsProm.stat(`./03-files-in-folder/secret-folder/${result[i][0]}.${result[i][1]}`);
+        fileInfo.then(data => {
+            console.log(`${result[i][0]} - ${result[i][1]} - ${(data.size/1024).toFixed(3)}kb`);
+        });
     }
 });
